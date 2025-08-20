f779fecd81d0cb18a46451289fe09f3cd2374cd0
Shinya Sakae <ss49919201@gmail.com>
2025-08-20 23:29:01 +0900

    feat: date functions

---
+++ app/analyzer/internal/date/date.go
@@ -0,0 +1,15 @@
+package date
+
+import "time"
+
+func BeginningOfDay(t time.Time) time.Time {
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
+}
+
+func EndOfDay(t time.Time) time.Time {
+	return time.Date(t.Year(), t.Month(), t.Day(), 23, 59, 59, 999999999, t.Location())
+}
+
+func AddDays(t time.Time, days int) time.Time {
+	return t.AddDate(0, 0, days)
+}
