3744faf430fbb0f5542d77b9d5d49f5102352ef5
Shinya Sakae <ss49919201@gmail.com>
2025-08-21 07:38:24 +0900

    feat: implement analyze usecase

--- app/analyzer/internal/port/usecase/analyze.go
+++ app/analyzer/internal/port/usecase/analyze.go
@@ -6,7 +6,20 @@ import (
 	"github.com/samber/mo"
 )
 
-type AnalyzeInput struct{}
-type AnalyzeOutput struct{}
+type GoalType int
+
+const (
+	RecentWeek  GoalType = iota + 1
+	RecentMonth GoalType = iota + 1
+)
+
+type AnalyzeInput struct {
+	Goal GoalType
+}
+type AnalyzeOutput struct {
+	IsGoalAchieved bool
+
+	// TODO: LatestEntryPublishedAt mo.Option[time.Time]
+}
 
 type Analyze = func(context.Context, *AnalyzeInput) mo.Result[*AnalyzeOutput]
--- app/analyzer/internal/usecase/analyze.go
+++ app/analyzer/internal/usecase/analyze.go
@@ -1,1 +1,38 @@
 package usecase
+
+import (
+	"context"
+	"time"
+
+	"github.com/samber/lo"
+	"github.com/samber/mo"
+	"github.com/ss49919201/fight-op/app/analyzer/internal/date"
+	"github.com/ss49919201/fight-op/app/analyzer/internal/port/fetcher"
+	"github.com/ss49919201/fight-op/app/analyzer/internal/port/usecase"
+)
+
+func NewAnalyze(fetchAllByDate fetcher.FetchAllByDate) usecase.Analyze {
+	return func(ctx context.Context, in *usecase.AnalyzeInput) mo.Result[*usecase.AnalyzeOutput] {
+		return analyze(ctx, in, fetchAllByDate)
+	}
+}
+
+func analyze(ctx context.Context, in *usecase.AnalyzeInput, fetchAllByDate fetcher.FetchAllByDate) mo.Result[*usecase.AnalyzeOutput] {
+	now := time.Now()
+	beginningOfToday := date.BeginningOfDay(now)
+	beginningOfBeforeXdays := date.AddDays(
+		beginningOfToday,
+		lo.If(in.Goal == usecase.RecentWeek, -7).
+			ElseIf(in.Goal == usecase.RecentMonth, -30).
+			Else(-7),
+	)
+
+	fetched := fetchAllByDate(ctx, beginningOfBeforeXdays, beginningOfToday)
+	if fetched.IsError() {
+		return mo.Err[*usecase.AnalyzeOutput](fetched.Error())
+	}
+
+	return mo.Ok(&usecase.AnalyzeOutput{
+		IsGoalAchieved: len(fetched.MustGet()) > 0,
+	})
+}
