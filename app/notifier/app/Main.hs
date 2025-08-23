45391aa89798e1040d61ef5938134589c0f91f93
Shinya Sakae <ss49919201@gmail.com>
2025-08-23 13:02:10 +0900

    feat: add notifier

--- app/notifier/app/Main.hs
+++ app/notifier/app/Main.hs
@@ -1,6 +1,68 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+
+import Data.Aeson
+import Data.Aeson.Types (parseMaybe)
+import Data.ByteString.Lazy qualified as LBS
+import GHC.Generics (Generic)
+import Network.HTTP.Simple
+import System.Environment (lookupEnv)
+
+newtype ReqBody = ReqBody {text :: String}
+  deriving (Generic, Show)
+
+instance ToJSON ReqBody
+
+getIsGoalAchieved :: Object -> Maybe Bool
+getIsGoalAchieved = parseMaybe $ \obj -> obj .: "is_goal_achieved"
+
+reqBody :: Bool -> ReqBody
+reqBody True = ReqBody "目標達成です🎊よく頑張りました！"
+reqBody False = ReqBody "目標未達です😢これから頑張りましょう！"
+
+loadSlackWebhookUrl :: IO (Maybe String)
+loadSlackWebhookUrl = do
+  maybeUrl <- lookupEnv "SLACK_WEBHOOK_URL"
+  return $ case maybeUrl of
+    Just url | not (null url) -> Just url
+    _ -> Nothing
+
+sendReq :: ReqBody -> IO (Either String ())
+sendReq reqBody = do
+  maybeUrl <- loadSlackWebhookUrl
+  case maybeUrl of
+    Nothing -> do
+      return $ Left "slack webhook url is not set or empty"
+    Just url -> do
+      request <- parseRequest $ "POST " ++ url
+      let requestWithBody = setRequestBodyJSON reqBody request
+      response <- httpLBS requestWithBody
+      let status = getResponseStatusCode response
+      print status
+      if status == 200
+        then do
+          print "success request"
+          return $ Right ()
+        else do
+          let errorMsg = "failed to request with status code: " ++ show status
+          putStrLn errorMsg
+          putStrLn $ "Response body: " ++ show (getResponseBody response)
+          return $ Left errorMsg
+
+notify :: Object -> IO ()
+notify value = do
+  case getIsGoalAchieved value of
+    Just isGoalAchieved -> do
+      result <- sendReq (reqBody isGoalAchieved)
+      either (putStrLn . ("sendReq failed: " ++)) (const $ return ()) result
+    Nothing -> putStrLn "failed to get isGoalAchieved"
 
 main :: IO ()
 main = do
-  s <- getLine
-  print s
+  decoded <- decode <$> LBS.getContents
+  case (decoded :: Maybe Object) of
+    Just value -> notify value
+    Nothing -> putStrLn "failed to decode json string."
