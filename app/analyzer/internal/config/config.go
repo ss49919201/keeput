865fe04c99b0e62759b6c45b823ab58533c7813b
Shinya Sakae <ss49919201@gmail.com>
2025-08-24 11:13:09 +0900

    feat: load feed url zenn from env

--- app/analyzer/internal/config/config.go
+++ app/analyzer/internal/config/config.go
@@ -1,6 +1,14 @@
 package config
 
-// TODP: load config
+import (
+	"os"
+	"sync"
+)
+
+var feedURLZenn = sync.OnceValue(func() string {
+	return os.Getenv("FEED_URL_ZENN")
+})
+
 func FeedURLZenn() string {
-	return "https://zenn.dev/ss49919201/feed"
+	return feedURLZenn()
 }
