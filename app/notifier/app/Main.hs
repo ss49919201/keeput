c792e5f79b3d9fe5c0eca4fe3e167f9408f62697
Shinya Sakae <ss49919201@gmail.com>
2025-08-29 07:54:09 +0900

    refactor: add usecase layer

--- app/notifier/app/Main.hs
+++ app/notifier/app/Main.hs
@@ -7,75 +7,7 @@ module Main where
 import Data.Aeson
 import Data.Aeson.Types (parseMaybe)
 import Data.ByteString.Lazy qualified as LBS
-import GHC.Generics (Generic)
-import Network.HTTP.Simple
-import System.Environment (lookupEnv)
-
-newtype ReqBody = ReqBody {text :: String}
-  deriving (Generic)
-
-instance ToJSON ReqBody
-
-getIsGoalAchieved :: Object -> Maybe Bool
-getIsGoalAchieved = parseMaybe $ \obj -> obj .: "is_goal_achieved"
-
-reqBody :: Bool -> ReqBody
-reqBody True = ReqBody "目標達成です🎊よく頑張りました！"
-reqBody False = ReqBody "目標未達です😢これから頑張りましょう！"
-
-loadValueFromEnv :: String -> IO (Maybe String)
-loadValueFromEnv key = do
-  maybeUrl <- lookupEnv key
-  return $ case maybeUrl of
-    Just url | not (null url) -> Just url
-    _ -> Nothing
-
-loadSlackWebhookUrl :: IO (Maybe String)
-loadSlackWebhookUrl = loadValueFromEnv "SLACK_WEBHOOK_URL"
-
-notificationCondition :: IO (Maybe String)
-notificationCondition = do
-  loadValueFromEnv "NOTIFICATION_CONDITION"
-
-data NotificationCondition
-  = OnFailure
-  | OnSuccess
-  | Always
-
-parseNotificationCondition :: Maybe String -> NotificationCondition
-parseNotificationCondition (Just "on_failure") = OnFailure
-parseNotificationCondition (Just "on_success") = OnFailure
-parseNotificationCondition _ = Always
-
-sendReq :: ReqBody -> IO (Either String ())
-sendReq reqBody = do
-  maybeUrl <- loadSlackWebhookUrl
-  case maybeUrl of
-    Nothing -> do
-      return $ Left "slack webhook url is not set or empty"
-    Just url -> do
-      request <- parseRequest $ "POST " ++ url
-      let requestWithBody = setRequestBodyJSON reqBody request
-      response <- httpLBS requestWithBody
-      let status = getResponseStatusCode response
-      print status
-      if status == 200
-        then do
-          print "success request"
-          return $ Right ()
-        else do
-          let errorMsg = "failed to request with status code: " ++ show status
-          putStrLn errorMsg
-          putStrLn $ "Response body: " ++ show (getResponseBody response)
-          return $ Left errorMsg
-
-notify :: Object -> IO ()
-notify value = do
-  case getIsGoalAchieved value of
-    Just isGoalAchieved -> do
-      result <- sendReq (reqBody isGoalAchieved)
-      either (putStrLn . ("sendReq failed: " ++)) (const $ return ()) result
-    Nothing -> putStrLn "failed to get isGoalAchieved"
+import Usecase.Notifier
 
 getJsonFromStdio :: IO (Maybe Object)
 getJsonFromStdio = decode <$> LBS.getContents
