b69600ffd944ea43e486a069eaa5d34e3c5ffc29
Shinya Sakae <ss49919201@gmail.com>
2025-09-14 17:02:40 +0900

    feat(locker): add locker

---
+++ app/locker/src/index.ts
@@ -0,0 +1,48 @@
+import { vValidator } from "@hono/valibot-validator";
+import { Hono } from "hono";
+import { object, string } from "valibot";
+import { Locker } from "./locker";
+
+type Bindings = {
+  LOCKER: DurableObjectNamespace<Locker>;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.post(
+  "/acquire",
+  vValidator(
+    "json",
+    object({
+      lockId: string(),
+    })
+  ),
+  async (c) => {
+    const { lockId } = c.req.valid("json");
+    const stub = c.env.LOCKER.get(c.env.LOCKER.idFromName(lockId));
+    const result = await stub.acquire();
+    return c.json({
+      msg: result ? "ok" : "ng",
+    });
+  }
+);
+
+app.post(
+  "/release",
+  vValidator(
+    "json",
+    object({
+      lockId: string(),
+    })
+  ),
+  async (c) => {
+    const { lockId } = c.req.valid("json");
+    const stub = c.env.LOCKER.get(c.env.LOCKER.idFromName(lockId));
+    await stub.release();
+    return c.json({
+      msg: "ok",
+    });
+  }
+);
+
+export default app;
