444e6fe5278d8096e84b1a61ddabdc4b69945dc4
Shinya Sakae <ss49919201@gmail.com>
2025-10-18 12:17:18 +0900

    refactor(locker): ミドルウェアとエンドポイントでコードブロックを区切って整理

--- app/locker/src/index.ts
+++ app/locker/src/index.ts
@@ -12,60 +12,63 @@ type Bindings = {
 
 const app = new Hono<{ Bindings: Bindings }>();
 
-app.use(logger());
+// middleware
+{
+  app.use(logger());
+  app.use("*", async (c, next) => {
+    if (c.env.API_KEY === "") {
+      console.error("env api key is empty");
+      throw new HTTPException(500);
+    }
 
-app.use("*", async (c, next) => {
-  if (c.env.API_KEY === "") {
-    console.error("env api key is empty");
-    throw new HTTPException(500);
-  }
+    const apiKey = c.req.header("X-LOCKER-API-KEY");
+    if (apiKey !== c.env.API_KEY) {
+      throw new HTTPException(401);
+    }
+    await next();
+  });
+}
 
-  const apiKey = c.req.header("X-LOCKER-API-KEY");
-  if (apiKey !== c.env.API_KEY) {
-    throw new HTTPException(401);
-  }
-  await next();
-});
-
-app.get("health", (c) => {
-  return c.json("ok");
-});
-
-app.post(
-  "/acquire",
-  vValidator(
-    "json",
-    object({
-      lockId: string(),
-    })
-  ),
-  async (c) => {
-    const { lockId } = c.req.valid("json");
-    const stub = c.env.LOCKER.get(c.env.LOCKER.idFromName(lockId));
-    const result = await stub.acquire();
-    return c.json({
-      msg: result ? "ok" : "ng",
-    });
-  }
-);
-
-app.post(
-  "/release",
-  vValidator(
-    "json",
-    object({
-      lockId: string(),
-    })
-  ),
-  async (c) => {
-    const { lockId } = c.req.valid("json");
-    const stub = c.env.LOCKER.get(c.env.LOCKER.idFromName(lockId));
-    await stub.release();
-    return c.json({
-      msg: "ok",
-    });
-  }
-);
+// endpoint
+{
+  app.get("health", (c) => {
+    return c.json("ok");
+  });
+  app.post(
+    "/acquire",
+    vValidator(
+      "json",
+      object({
+        lockId: string(),
+      })
+    ),
+    async (c) => {
+      const { lockId } = c.req.valid("json");
+      const stub = c.env.LOCKER.get(c.env.LOCKER.idFromName(lockId));
+      const result = await stub.acquire();
+      return c.json({
+        msg: result ? "ok" : "ng",
+      });
+    }
+  );
+  app.post(
+    "/release",
+    vValidator(
+      "json",
+      object({
+        lockId: string(),
+      })
+    ),
+    async (c) => {
+      const { lockId } = c.req.valid("json");
+      const stub = c.env.LOCKER.get(c.env.LOCKER.idFromName(lockId));
+      await stub.release();
+      return c.json({
+        msg: "ok",
+      });
+    }
+  );
+}
 
 export default app;
 export { Locker };
