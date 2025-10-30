ab5f6737c8be03e6a42abea85d3116f284984692
Shinya Sakae <ss49919201@gmail.com>
2025-10-31 06:43:16 +0900

    feat(analyzer): TraceProvider初期化をLambda初期化フェーズに移動

--- app/analyzer/cmd/awslambda/main.go
+++ app/analyzer/cmd/awslambda/main.go
@@ -15,11 +15,6 @@ import (
 	"github.com/ss49919201/keeput/app/analyzer/internal/port/usecase"
 	"github.com/ss49919201/keeput/app/analyzer/internal/registory"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-lambda-go/otellambda"
-	"go.opentelemetry.io/otel"
-)
-
-const (
-	traceName = "github.com/ss49919201/keeput/app/cmd/awslambda"
 )
 
 func init() {
@@ -31,21 +26,6 @@ func init() {
 }
 
 func handleRequest(ctx context.Context) (err error) {
-	// fmt.Printf("hello\n")
-	// {
-	// 	req, err := http.NewRequest("GET", "localhost:4318", nil)
-	// 	if err != nil {
-	// 		fmt.Println(err)
-	// 	}
-	// 	resp, err := http.DefaultClient.Do(req)
-	// 	if err != nil {
-	// 		fmt.Println(err)
-	// 	} else {
-	// 		b, _ := io.ReadAll(resp.Body)
-	// 		fmt.Printf("status=%d,body=%s\n", resp.StatusCode, b)
-	// 	}
-	// }
-
 	defer func() {
 		if err != nil {
 			appotel.RecordError(ctx, err)
@@ -53,19 +33,6 @@ func handleRequest(ctx context.Context) (err error) {
 	}()
 
 	ctx = appctx.SetNow(ctx, time.Now())
-
-	shutdownTraceProvider, err := appotel.InitTraceProvider(ctx)
-	if err != nil {
-		slog.Error("failed to construct otel trace provider", slog.String("error", err.Error()))
-	}
-	defer func() {
-		if err := shutdownTraceProvider(ctx); err != nil {
-			slog.Warn("failed to shutdown trace provider", slog.String("error", err.Error()))
-		}
-	}()
-
-	ctx, span := otel.Tracer(traceName).Start(ctx, "Lambda Entrypoint")
-	defer span.End()
 
 	analyze, err := registory.NewAnalyzeUsecase(ctx)
 	if err != nil {
@@ -82,5 +49,15 @@ func handleRequest(ctx context.Context) (err error) {
 }
 
 func main() {
+	ctx := context.Background()
+	shutdownTraceProvider, err := appotel.InitTraceProvider(ctx)
+	if err != nil {
+		slog.Error("failed to construct otel trace provider", slog.String("error", err.Error()))
+	}
+	defer func() {
+		if err := shutdownTraceProvider(ctx); err != nil {
+			slog.Warn("failed to shutdown trace provider", slog.String("error", err.Error()))
+		}
+	}()
 	lambda.Start(otellambda.InstrumentHandler(handleRequest))
 }
