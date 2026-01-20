7020292c41f84a269cec533873135a9998d1004f
Shinya Sakae <ss49919201@gmail.com>
2026-01-21 07:52:03 +0900

    feat: add check constraint

--- rdb/schema.sql
+++ rdb/schema.sql
@@ -2,6 +2,6 @@ CREATE TABLE entries (
 	id              INTEGER PRIMARY KEY AUTOINCREMENT, 
     title           TEXT NOT NULL,
     published_at    INTEGER NOT NULL,
-    platform        TEXT NOT NULL,
+    platform        TEXT CHECK(platform = 'hatena' OR platform = 'zenn') NOT NULL,
     UNIQUE (platform, published_at, title)
 );
